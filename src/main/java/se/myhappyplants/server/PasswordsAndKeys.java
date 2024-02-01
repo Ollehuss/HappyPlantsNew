bb7a587785fcba1ff8f522e0cfadf1ae87362c88
Casper Johannesson <casperjohannesson@hotmail.com>
2024-02-01 14:56:59 +0100

    Merging Omars branch to master

--- src/main/java/se/myhappyplants/server/PasswordsAndKeys.java
+++ src/main/java/se/myhappyplants/server/PasswordsAndKeys.java
@@ -3,11 +3,11 @@ package se.myhappyplants.server;
 //THIS FILE IS HIDDEN FROM GIT
 public class PasswordsAndKeys {
     //DB
-    public static String dbServerIp = "Myhappyplants.database.windows.net";
-    public static String dbServerPort = "1433";
-    public static String dbUsername = "mhpadmin";
-    public static String dbPassword = "Viälskarsven123";
-    public static String dbHostName = "x";
+    public static String dbServerIp = "pgserver.mau.se";
+    public static String dbServerPort = "5432";
+    public static String dbUsername = "an5859";
+    public static String dbPassword = "vv0a80kd";
+    public static String dbHostName = "pgserver.mau.se";
 
     //API
     public static String APIToken = "eI01vwK-LgBiMpuVI3tqDaT7xKSEyoEl2qf20rwxb9k&q";
